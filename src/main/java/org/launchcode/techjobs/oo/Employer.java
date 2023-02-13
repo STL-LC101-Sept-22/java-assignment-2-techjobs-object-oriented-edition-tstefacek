8e7c8f02e180b05f0423cff5bd79a0a51972188d
Tashe <tstefacek92@gmail.com>
2023-02-13 17:33:30 -0600

    Refactored to DRY the support classes. Created aJobField class, moved all shared fields, getters/setters, and custom method modified other classes to reference shared code.

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -2,31 +2,11 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class Employer {
+public class Employer extends JobField{
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    //increments nexId so every new Employer object gets a different ID number
-    public Employer() {
-        id = nextId;
-        nextId++;
-    }
-    /*assigns aValue to value AND initializes if by calling the first constructor
-    with the this() statement */
     public Employer(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
-
-    // Custom toString, equals, and hashCode methods:
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
     @Override
     public boolean equals(Object o) {  // Two objects are equal if they have the same id.
         if (this == o) return true;
@@ -35,23 +15,6 @@ public class Employer {
         return getId() == employer.getId();
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 
 }
